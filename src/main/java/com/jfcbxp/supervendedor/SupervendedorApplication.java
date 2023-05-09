25abbab60d15f7b242561b692b4a39b77758c35e
Joao Braga <jfcbxp@gmail.com>
2023-05-09 17:19:47 -0300

    projeto iniciado

---
+++ src/main/java/com/jfcbxp/supervendedor/SupervendedorApplication.java
@@ -0,0 +1,13 @@
+package com.jfcbxp.supervendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervendedorApplication.class, args);
+	}
+
+}
