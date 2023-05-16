6501dcdbefa619e375277dfad2a0bf66cfccadde
Joao Braga <jfcbxp@gmail.com>
2023-05-16 19:31:20 -0300

    update Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 source version.properties
-echo "This is the entrypoint script executing jar file: $IMAGE_VERSION"
+echo "This is the entrypoint script executing jar file: $ARTIFACT_NAME"
 echo "Image version: $IMAGE_VERSION"
-java -jar "$IMAGE_VERSION.jar"
+java -jar "supervendedor.jar"
